8b1802bb547641625805247beea66e0fcbd68416
Toniddarden1 <Toniddarden@gmail.com>
2020-02-17 23:18:40 -0500

    done gd pj

--- api/server.js
+++ api/server.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-const authRouter = require('../auth/auth-router.js');
-const usersRouter = require('../users/users-router.js');
+const authRouter = require("../auth/auth-router.js");
+const usersRouter = require("../users/users-router.js");
 
 const server = express();
 
@@ -11,11 +12,28 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
-server.get('/', (req, res) => {
+server.get("/", (req, res) => {
   res.send("It's alive!");
+});
+
+// example token
+server.get("/token", (req, res) => {
+  const payload = {
+    subject: "user.id",
+    username: "user.username",
+    favdog: "moofi"
+  };
+  const secret = "thekingisgalaxy";
+
+  const options = {
+    expiresIn: "8h"
+  };
+
+  const token = jwt.sign(payload, secret, options);
+  res.json(token);
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secret = require('../config/secrets');
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 // for endpoints beginning with /api/auth
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   const hash = bcrypt.hashSync(user.password, 10); // 2 ^ n
   user.password = hash;
@@ -18,23 +20,42 @@ router.post('/register', (req, res) => {
     });
 });
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // add token function
+        const token = genToken(user);
         res.status(200).json({
           message: `Welcome ${user.username}!`,
+          token: token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
 });
+
+function genToken(user) {
+  const payload = {
+    userid: user.id,
+    username: user.username
+    // other thing: to add
+  };
+
+  const options = {
+    expiresIn: "1h"
+  };
+
+  const token = jwt.sign(payload, secret.jwtSecret, options);
+
+  return token;
+}
 
 module.exports = router;
